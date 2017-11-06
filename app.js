0c4f35df53aef2e2eee544e952f2f74d7472f5cd
syedraza04 <syedraza04@gmail.com>
2017-11-06 12:26:43 -0500

    yargs methods

--- app.js
+++ app.js
@@ -2,38 +2,41 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs=require('yargs');
 
 const notes = require('./notes.js');
 
-let command = process.argv[2];
-console.log(command);
-let flag = 0;
+const command = process.argv[2];
+const argv = yargs.argv;
 
+console.log('Yargs',argv);
+console.log('Args',process.argv);
+// let flag = 0;
+//
 const loggingFunc = (input) => {
     console.log(input);
 };
-
+//
 const permittedCommands = ['add','list','read','remove'];
 
 permittedCommands.map((values)=>{
-    if(command.indexOf(values) === -1 && flag === 0){
+    if(command.indexOf(values) === -1 ){
     console.log('Command not found');
-    flag++;
-   }else{
-
-        switch(command){
-            case 'add':
-                loggingFunc('adding new note');
-                break;
-            case 'list':
-                loggingFunc('Listing all notes');
-                break;
-            case 'read':
-                loggingFunc('Reading note');
-                break;
-            case 'remove':
-                loggingFunc('Removing note');
-                break;
+    // flag++;
+    } else{
+       switch(command){
+          case 'add':
+             notes.addNote(argv.title,argv.body);
+             break;
+          case 'list':
+             notes.getAll();
+             break;
+          case 'read':
+             notes.readNote(argv.title);
+             break;
+          case 'remove':
+             notes.removeNote(argv.title);
+             break;
         }
     }
 
--- notes.js
+++ notes.js
@@ -1,6 +1,26 @@
 // console.log('starting notes.js');
-module.exports.age = 25;
-module.exports.addNote = (a,b) => {
-  return a+b;
+
+const addNote = (title,body) => {
+    console.log('Adding note: ',title,body);
 };
 
+const readNote = (title) => {
+    console.log('Reading Note: ');
+    console.log(title)
+};
+
+const removeNote = (title) => {
+    console.log('Removing Note');
+    console.log(title)
+};
+
+var getAll = () => {
+    console.log('getting all notes');
+}
+
+module.exports = {
+    addNote,
+    readNote,
+    removeNote,
+    getAll
+};
